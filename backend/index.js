a3902590a0c02b1704abb1a818e24db3afc7595f
I4MGlitch <91672477+I4MGlitch@users.noreply.github.com>
2024-08-28 16:01:58 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(parser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // MongoDB Connection using Mongoose
-mongoose.connect('mongodb+srv://mrxstylers:mrxstylers@tahura.ydoqsiv.mongodb.net/')
+mongoose.connect('mongodb+srv://mrxstylers:gonzo112233@tahura.ydoqsiv.mongodb.net/')
   .then(() => console.log('MongoDB connected to database: TAHURA'))
   .catch(err => console.error('MongoDB connection error:', err));
 
